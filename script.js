e56e2bd3f4556dcc4452efe8be5cfef006a43710
Grassper <rishi.bharath27@gmail.com>
2020-12-05 10:03:18 +0530

    implement dynamic width, current time, duration ✔️

--- script.js
+++ script.js
@@ -32,7 +32,22 @@ video.addEventListener('ended',showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time){
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+
+
+// update progress bar as video plays
+function updateProgress(){
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
 
 // Volume Controls --------------------------- //
 
@@ -47,3 +62,5 @@ video.addEventListener('ended',showPlayIcon);
 // Event listener
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
